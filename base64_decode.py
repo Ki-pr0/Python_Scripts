a0f90d84754ad26bd69f3c67f49a31253b7d18d5
K0Hack <82372292+Ki-pr0@users.noreply.github.com>
2021-07-29 02:46:21 +0200

    Create base64_decode.py

---
+++ base64_decode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Script para de codear unas 13 veces una cadena en base64 en la maquina Poison de HTB
+from base64 import b64decode
+
+pass_en = "CADENA EN BASE64 A DECODEAR"
+# bucle para decodear X veces
+for i in range(13):
+  pass_en = b64decode(pass_en)
+  
+print(pass_en)
